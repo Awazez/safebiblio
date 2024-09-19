67fc37583e0b991d6eae20c5006601216a2fd8d7
Awazez <martin.aubeut@gmail.com>
2024-09-19 19:09:55 +0200

    initial commit

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,40 @@
+<script setup>
+
+
+
+</script>
+
+<template>
+
+<section>Construire une safe place pour votre bibliothèque</section>
+<button>Télécharger le protocole</button> <br>
+<p>Participez ! Envoyer un mail à contact@safebiblio.fr</p>
+</template>
+
+<style scoped>
+
+@import url('https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,100..900;1,100..900&display=swap');
+
+section {
+  font-size: 3em;
+  font-family: "Montserrat", system-ui;
+  font-weight: 900;
+  font-style: bold italic;
+  color: black;
+}
+
+p {
+  font-family: "Montserrat", system-ui;
+  font-weight: 900;
+  color: black;
+}
+
+button {
+  background: white;
+  color: black;
+}
+
+
+
+
+</style>
