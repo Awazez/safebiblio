6f3a771df37f78ece52c544c1f8d35425c960fa9
cherbourg@fub.fr <cherbourg@fub.fr>
2024-09-19 20:35:55 +0200

    add favicon on html

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,18 +1,20 @@
 <script setup>
-
-
-
+// Tu peux ajouter des scripts ici si tu as besoin de manipuler plus de logique avec Vue
 </script>
 
 <template>
-
-<section>Construire une safe place pour votre bibliothèque</section>
-<button>Télécharger le protocole</button> <br>
-<p>Participez ! Envoyer un mail à contact@safebiblio.fr</p>
+  <section>Construire une safe place pour votre bibliothèque</section>
+  
+  <!-- Ajouter un lien pour déclencher le téléchargement -->
+  <a href="/public/Protocole_Safe_Zone.pdf" download="Protocole_Safe_Biblio.pdf">
+    <button>Télécharger le protocole</button>
+  </a> 
+  
+  <br>
+  <p>Participez ! Envoyer un mail à contact@safebiblio.fr</p>
 </template>
 
 <style scoped>
-
 @import url('https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,100..900;1,100..900&display=swap');
 
 section {
@@ -32,9 +34,16 @@ p {
 button {
   background: white;
   color: black;
+  border: none;
+  padding: 10px 20px;
+  font-family: "Montserrat", system-ui;
+  font-weight: 700;
+  cursor: pointer;
 }
 
-
-
-
+button:hover {
+  background-color: #000;
+  color: #fff;
+}
 </style>
+
